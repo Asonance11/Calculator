ba45951de335a4d291c1957c62ed12833f3be4da
Oluwadabira <adigunsamuelaso@gmail.com>
2022-11-22 12:00:04 +0100

    start of JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const previousOperation = document.getElementById(previousOp);
+const currentOperation = document.getElementById(currentOp);
+const clearButton = document.getElementById(clearButton);
+const equalsButton = document.getElementById(equalsButton);
+const plusMinus = document.getElementById(plusMinus);
+const percent = document.getElementById(percent);
+const numberButtons = document.querySelectorAll([data - number]);
+const operationButtons = document.querySelectorAll([data - operation]);
