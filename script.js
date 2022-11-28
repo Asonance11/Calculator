43d773dc0477ef31b53f606a9addf851ee3f0a21
Oluwadabira <adigunsamuelaso@gmail.com>
2022-11-28 16:40:48 +0100

    addition of clear button

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-const previousOperation = document.getElementById(previousOp);
-const currentOperation = document.getElementById(currentOp);
-const clearButton = document.getElementById(clearButton);
-const equalsButton = document.getElementById(equalsButton);
-const plusMinus = document.getElementById(plusMinus);
-const percent = document.getElementById(percent);
-const numberButtons = document.querySelectorAll([data - number]);
-const operationButtons = document.querySelectorAll([data - operation]);
-let currentOp;
-let previousOp;
+const previousOperation = document.getElementById('previousOp');
+const currentOperation = document.getElementById('currentOp');
+const clearButton = document.getElementById('clearButton');
+const equalsButton = document.getElementById('equalsButton');
+const plusMinus = document.getElementById('plusMinus');
+const percent = document.getElementById('percent');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+//let currentOp;
+//let previousOp;
 
 function appendNumber(number) {
 	currentOperation.textContent += number;
 }
+function clear() {
+	currentOperation.textContent = '';
+	previousOperation.textContent = '';
+}
 numberButtons.forEach((button) => {
 	button.addEventListener('click', () => {
-		console.log(button.innerText);
+		appendNumber(button.textContent);
 	});
 });
+clearButton.addEventListener('click', clear);
